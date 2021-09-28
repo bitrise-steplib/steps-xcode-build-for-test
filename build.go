dfe3c35bac4af574ac8f3bc67760f7b2167adf3e
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2021-09-28 12:19:56 +0200

    Migrate to go modules. (#15)
    * Migrate to go modules.
    
    * Fix gitignore.
    
    * Delete unused output.go file.

--- build.go
+++ build.go
@@ -36,14 +36,16 @@ func runCommandWithRetry(cmd *xcodebuild.CommandBuilder, useXcpretty bool, swift
 	return output, buildInterval, err
 }
 
-func prepareCommand(xcodeCmd *xcodebuild.CommandBuilder, useXcpretty bool, output *bytes.Buffer) (*command.Model, *xcpretty.CommandModel) {
+func prepareCommand(xcodeCmd *xcodebuild.CommandBuilder, useXcpretty bool, output *bytes.Buffer) (command.Command, *xcpretty.CommandModel) {
 	if useXcpretty {
 		return nil, xcpretty.New(*xcodeCmd)
 	}
 
-	buildRootCmd := xcodeCmd.Command()
-	buildRootCmd.SetStdout(io.MultiWriter(os.Stdout, output))
-	buildRootCmd.SetStderr(io.MultiWriter(os.Stderr, output))
+	buildRootCmd := xcodeCmd.Command(&command.Opts{
+		Stdout: io.MultiWriter(os.Stdout, output),
+		Stderr: io.MultiWriter(os.Stderr, output),
+	})
+
 	return buildRootCmd, nil
 }
 
